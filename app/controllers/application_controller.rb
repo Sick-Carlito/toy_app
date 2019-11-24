926b441bb4fc0d17584d29d73a3fd124cfa8d37d
sick-carlito <carlb1319@gmail.com>
2019-11-24 12:04:25 +0300

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
+#app/controllers/application_controller.
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
 end
